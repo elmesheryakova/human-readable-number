dd9b3c1645c520d7cfa79fdb841d946a18e7ca7a
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-01-05 12:58:18 +0300

    feat: test v-2

--- src/index.js
+++ src/index.js
@@ -3,41 +3,30 @@ module.exports = function toReadable(number) {
     var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
-    if (number >= 1000000) {
-        return (Math.floor(number / 1000000)) + " million " + convert_thousands(number % 1000000);
+
+
+    if (number == 0) return "zero";
+
+    if (number >= 1000) {
+        return convert_hundreds(Math.floor(number / 1000)).trim() + " thousand " + convert_hundreds(number % 1000).trim();
     } else {
-        return convert_thousands(number);
-    }
-
-
-
-    function convert_thousands(number) {
-        if (number >= 1000) {
-            return convert_hundreds(Math.floor(number / 1000)) + " thousand " + convert_hundreds(number % 1000);
-        } else {
-            return convert_hundreds(number);
-        }
+        return convert_hundreds(number).trim();
     }
 
     function convert_hundreds(number) {
         if (number > 99) {
-            return ones[Math.floor(number / 100)] + " hundred " + convert_tens(number % 100);
+            return ones[Math.floor(number / 100)].trim() + " hundred " + convert_tens(number % 100).trim();
         } else {
-            return convert_tens(number);
+            return convert_tens(number).trim();
         }
     }
 
     function convert_tens(number) {
-        if (number < 10) return ones[number];
-        else if (number >= 10 && number < 20) return teens[number - 10];
+        if (number < 10) return ones[number].trim();
+        else if (number >= 10 && number < 20) return teens[number - 10].trim();
         else {
-            return tens[Math.floor(number / 10)] + " " + ones[number % 10];
+            return tens[Math.floor(number / 10)].trim() + " " + ones[number % 10].trim();
         }
-    }
-
-    function convert(number) {
-        if (number == 0) return "zero";
-        // else return convert_millions(number);
     }
 
 
